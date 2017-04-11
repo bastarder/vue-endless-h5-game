0ebbacfb80731f7ee5a75fae4849d64c65aa4fc7
bastarder <85257684@qq.com>
2017-04-11 21:40:28 +0800

    add map data

--- src/data/map-data.js
+++ src/data/map-data.js
@@ -37,6 +37,156 @@ const MAP_TABLE = [
     }
     
   },
+  {
+    id: 8000003,
+    name: '青木镇',
+    logo: '',
+    dsc: '繁华的小镇,却影藏着危机...',
+    eventList: [],    
+    monsterList: [],  
+    itemList: [       
+
+    ],
+    rule: {
+
+    }
+    
+  },
+  {
+    id: 8000004,
+    name: '镇外',
+    logo: '',
+    dsc: '发狂的人类?...',
+    eventList: [],    
+    monsterList: [],  
+    itemList: [       
+
+    ],
+    rule: {
+
+    }
+    
+  },
+  {
+    id: 8000005,
+    name: '青木林',
+    logo: '',
+    dsc: '发狂的人类?...',
+    eventList: [],    
+    monsterList: [],  
+    itemList: [       
+
+    ],
+    rule: {
+
+    }
+    
+  },
+  {
+    id: 8000006,
+    name: '火焰洞穴',
+    logo: '',
+    dsc: '发狂的人类?...',
+    eventList: [],    
+    monsterList: [],  
+    itemList: [       
+
+    ],
+    rule: {
+
+    }
+    
+  },
+  {
+    id: 8000007,
+    name: '冰霜洞穴',
+    logo: '',
+    dsc: '发狂的人类?...',
+    eventList: [],    
+    monsterList: [],  
+    itemList: [       
+
+    ],
+    rule: {
+
+    }
+    
+  },
+  {
+    id: 8000008,
+    name: '祭祀台',
+    logo: '',
+    dsc: '发狂的人类?...',
+    eventList: [],    
+    monsterList: [],  
+    itemList: [       
+
+    ],
+    rule: {
+
+    }
+    
+  },
+  {
+    id: 8000009,
+    name: '骷髅大厅',
+    logo: '',
+    dsc: '发狂的人类?...',
+    eventList: [],    
+    monsterList: [],  
+    itemList: [       
+
+    ],
+    rule: {
+
+    }
+    
+  },
+  {
+    id: 80000010,
+    name: '骷髅王',
+    logo: '',
+    dsc: '发狂的人类?...',
+    eventList: [],    
+    monsterList: [],  
+    itemList: [       
+
+    ],
+    rule: {
+
+    }
+    
+  },
+  {
+    id: 80000011,
+    name: '洞穴出口',
+    logo: '',
+    dsc: '发狂的人类?...',
+    eventList: [],    
+    monsterList: [],  
+    itemList: [       
+
+    ],
+    rule: {
+
+    }
+    
+  },
+  {
+    id: 80000012,
+    name: '黑色小溪',
+    logo: '',
+    dsc: '发狂的人类?...',
+    eventList: [],    
+    monsterList: [],  
+    itemList: [       
+
+    ],
+    rule: {
+
+    }
+    
+  },
 ];
 
 
